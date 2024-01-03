3445ed9b5a4149d8ec18afd8056d07c34d369431
Yat Fu Chan <yatfuchan@Yats-MacBook-Pro.local>
2024-01-03 16:35:40 +0800

    added dragging mechanism to bpm input

--- src/components/PianoRoll/components/TempoInfo/index.tsx
+++ src/components/PianoRoll/components/TempoInfo/index.tsx
@@ -8,7 +8,11 @@ export default function TempoInfo() {
   const { pianoRollStore } = useStore();
   const dispatch = usePianoRollDispatch();
 
+  const [isDragging, setIsDragging] = useState(false);
+  const [inintialY, setInitialY] = useState(0);
+
   const [inputValue, setInputValue] = useState(pianoRollStore.bpm.toString());
+  const bufferedValue = useRef(inputValue);
 
   useEffect(() => {
     setInputValue(pianoRollStore.bpm.toString());
@@ -22,9 +26,28 @@ export default function TempoInfo() {
     event?.currentTarget.focus()
   }
 
-  const handlePointerDown: React.MouseEventHandler<HTMLInputElement> = (event) => {
+  const handlePointerDown: React.PointerEventHandler<HTMLInputElement> = (event) => {
     event.preventDefault()
+    event.currentTarget.setPointerCapture(event.pointerId);
+    setIsDragging(true);
+    setInitialY(event.clientY);
+    bufferedValue.current = inputValue;
   }
+
+  const handlePointerMove: React.PointerEventHandler<HTMLInputElement> = (event) => {
+    if (isDragging) {
+      const newValue = parseFloat(bufferedValue.current) - (event.clientY - inintialY)
+      const clampedValue = Math.max(40, Math.min(200, newValue));
+      setInputValue(Math.round(clampedValue).toString());
+    }
+  }
+
+  const handlePointerUp: React.PointerEventHandler<HTMLInputElement> = (event) => {
+    event.currentTarget.releasePointerCapture(event.pointerId);
+    setIsDragging(false);
+  }
+
+
 
   const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (event) => {
     if (event.key === 'Enter') {
@@ -44,6 +67,7 @@ export default function TempoInfo() {
     <input className={styles['tempo-info']} value={inputValue}
       onChange={handleChange} onKeyDown={handleKeyDown}
       onPointerDown={handlePointerDown} onDoubleClick={handleDoubleClick}
+      onPointerMove={handlePointerMove} onPointerUp={handlePointerUp}
     />
   );
 }
