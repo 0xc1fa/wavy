8c7bb56120c876fa2b8090adc1c51c01f8cbec2e
Yat Fu Chan <chanyatfu0616@gmail.com>
2024-01-28 19:28:21 +0800

    Updated package export

--- packages/react-piano-roll/src/index.ts
+++ packages/react-piano-roll/src/index.ts
@@ -3,3 +3,5 @@ export { PianoRollStoreProvider } from './store/pianoRollStore'
 export { default as useStore } from './hooks/useStore'
 export { usePianoRollNotes } from './helpers/notes'
 export { usePianoRollDispatch } from './hooks/usePianoRollDispatch'
+export { default as createMIDIFile } from './helpers/midi'
+export { default as usePreventZoom } from "./hooks/usePreventZoom";
