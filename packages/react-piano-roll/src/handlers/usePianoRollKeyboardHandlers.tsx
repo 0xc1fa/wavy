6242647d121d078f68c59f1af88fb2ebb5552ff1
Yat Fu Chan <chanyatfu0616@gmail.com>
2024-02-09 20:44:09 +0800

    Deleted unused handlers

--- packages/react-piano-roll/src/handlers/usePianoRollKeyboardHandlers.tsx
+++ packages/react-piano-roll/src/handlers/usePianoRollKeyboardHandlers.tsx
@@ -1,21 +1,14 @@
-import { useEffect, useReducer, useRef, useState } from "react";
+import { useRef } from "react";
 import { useStore } from "@/hooks/useStore";
-import { TrackNoteEvent } from "@/types/TrackNoteEvent";
 import {
-  focusNote,
-  getEndingTickFromNotes,
-  getSelectionRangeWithSelectedNotes,
-  getStartingTickFromNotes,
   useNotes,
 } from "../helpers/notes";
 import _ from "lodash";
-import { useClipboard, useClipboardReducer } from "@/hooks/useClipboard";
 
 
 export default function usePianoRollKeyboardHandlers(onSpace?: (event: React.KeyboardEvent) => void) {
   const { pianoRollStore, dispatch } = useStore();
   const notes = useNotes();
-  const { clipboard, clipboardDispatch } = useClipboardReducer();
 
   let spaceDown = useRef(false);
 
@@ -31,16 +24,6 @@ export default function usePianoRollKeyboardHandlers(onSpace?: (event: React.Key
     }
     if (event.metaKey) {
       switch (event.code) {
-        // case "KeyX":
-        //   onCopy(event);
-        //   dispatch({ type: "DELETE_SELECTED_NOTES" });
-        //   break;
-        // case "KeyC":
-        //   onCopy(event);
-        //   break;
-        // case "KeyV":
-        //   onPaste(event);
-        //   break;
         case "KeyZ": {
           if (event.shiftKey) {
             dispatch({ type: "REDO" });
@@ -81,54 +64,6 @@ export default function usePianoRollKeyboardHandlers(onSpace?: (event: React.Key
 
   const onSpaceDown = (event: React.KeyboardEvent) => {
     onSpace?.(event);
-  };
-
-  const onCopy = (event: React.KeyboardEvent) => {
-    event.preventDefault();
-    event.stopPropagation();
-    if (pianoRollStore.selectedNotes().length === 0) {
-      return;
-    }
-
-    let selectionRange = getSelectionRangeWithSelectedNotes(
-      pianoRollStore.selectedNotes(),
-      pianoRollStore.selectionRange!,
-    );
-    clipboardDispatch({
-      type: "setNote",
-      payload: { notes: pianoRollStore.selectedNotes(), selectedRange: selectionRange },
-    });
-  };
-
-  const onPaste = (event: React.KeyboardEvent) => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    if (pianoRollStore.selectionTicks === null) {
-      return;
-    }
-
-    if (clipboard.notes.length === 0) {
-      return;
-    }
-    const shiftedNotes = clipboard.notes.map((note) => ({
-      ...note,
-      tick: pianoRollStore.selectionTicks! + note.tick,
-    }));
-    dispatch({ type: "UNSELECTED_ALL_NOTES" });
-    dispatch({ type: "ADD_NOTES", payload: { notes: shiftedNotes } });
-    dispatch({
-      type: "SET_SELECTION_TICKS",
-      payload: { ticks: pianoRollStore.selectionTicks! + clipboard.selectionWidth },
-    });
-    dispatch({
-      type: "SET_SELECTION_RANGE",
-      payload: {
-        range: pianoRollStore.selectionRange
-          ? (pianoRollStore.selectionRange.map((item) => item + clipboard.selectionWidth) as [number, number])
-          : null,
-      },
-    });
   };
 
   return {
