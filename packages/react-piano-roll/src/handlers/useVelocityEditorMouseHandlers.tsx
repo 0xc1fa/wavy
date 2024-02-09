bc46afecd3f4edd8669188854e24b32aca9c4566
Yat Fu Chan <chanyatfu0616@gmail.com>
2024-02-09 15:35:26 +0800

    Fixed velocity editor

--- packages/react-piano-roll/src/handlers/useVelocityEditorMouseHandlers.tsx
+++ packages/react-piano-roll/src/handlers/useVelocityEditorMouseHandlers.tsx
@@ -4,7 +4,6 @@ import { getNotesFromOffsetX } from "@/helpers/conversion";
 import { useScaleX } from "@/contexts/ScaleXProvider";
 import { TrackNoteEvent } from "@/types";
 import { getNoteIdFromEvent, getRelativeX, getRelativeY } from "@/helpers/event";
-import { get } from "lodash";
 
 enum VelocityEditorMouseHandlerMode {
   Idle,
@@ -28,36 +27,33 @@ export default function useVelocityEditorMouseHandlers() {
   const { pianoRollStore, dispatch } = useStore();
   const { scaleX } = useScaleX();
   const noteClicked = useRef<TrackNoteEvent | null>(null);
-  const modificationBuffer = useRef<Buffer>({
-    bufferedNotes: [],
-    bufferedY: 0,
-  });
 
   const onPointerDown: React.PointerEventHandler = (event) => {
     event.currentTarget.setPointerCapture(event.pointerId);
 
     noteClicked.current = getNoteObjectFromEvent(pianoRollStore.notes, event);
     if (!noteClicked.current) {
+      if (!event.shiftKey) {
+        dispatch({ type: "UNSELECTED_ALL_NOTES" });
+      }
       return;
     }
     if (mouseHandlerMode === VelocityEditorMouseHandlerMode.Pencil) {
     } else if (mouseHandlerMode === VelocityEditorMouseHandlerMode.SelectAndDrag) {
       if (noteClicked.current.isSelected) {
-        modificationBuffer.current = {
-          bufferedNotes: pianoRollStore.selectedNotes(),
-          bufferedY: getRelativeY(event),
-        };
       } else {
-        dispatch({ type: "UNSELECTED_ALL_NOTES" });
+        if (!event.shiftKey) {
+          dispatch({ type: "UNSELECTED_ALL_NOTES" });
+        }
         dispatch({
           type: "SET_NOTE_AS_SELECTED",
           payload: { noteId: noteClicked.current.id },
         });
-        modificationBuffer.current = {
-          bufferedNotes: [noteClicked.current],
-          bufferedY: getRelativeY(event),
-        };
       }
+      dispatch({
+        type: "SET_NOTE_MODIFICATION_BUFFER_WITH_SELECTED_NOTE",
+        payload: { initX: getRelativeX(event), initY: getRelativeY(event) },
+      });
     }
   };
 
@@ -67,29 +63,25 @@ export default function useVelocityEditorMouseHandlers() {
     }
     const containerHeight = event.currentTarget.clientHeight;
     const relativeY = getRelativeY(event);
-    const initVelocity = getVelocityByRelativeY(
-      containerHeight,
-      modificationBuffer.current.bufferedY,
-    );
-    const currentVelocity = getVelocityByRelativeY(
-      containerHeight,
-      relativeY
-    )
+    const initVelocity = getVelocityByRelativeY(containerHeight, pianoRollStore.noteModificationBuffer.initY);
+    const currentVelocity = getVelocityByRelativeY(containerHeight, relativeY);
     const deltaVelocity = currentVelocity - initVelocity;
     if (mouseHandlerMode === VelocityEditorMouseHandlerMode.Pencil) {
-      const notes = getNotesFromOffsetX(scaleX,pianoRollStore.notes, event.clientX);
+      const notes = getNotesFromOffsetX(scaleX, pianoRollStore.notes, event.clientX);
       const newNote = notes.map((note) => ({
         ...note,
         velocity: currentVelocity,
+        isSelected: true,
       }));
       dispatch({
         type: "MODIFYING_NOTES",
         payload: { notes: newNote },
       });
     } else if (mouseHandlerMode === VelocityEditorMouseHandlerMode.SelectAndDrag) {
-      const newNote = modificationBuffer.current.bufferedNotes.map((note) => ({
+      const newNote = pianoRollStore.noteModificationBuffer.notesSelected.map((note) => ({
         ...note,
         velocity: note.velocity + deltaVelocity,
+        isSelected: true,
       }));
       dispatch({
         type: "MODIFYING_NOTES",
