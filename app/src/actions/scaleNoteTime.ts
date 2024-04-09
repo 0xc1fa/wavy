3530ab4aebcb584294d7248135aa255e96e268d2
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-04-10 06:17:30 +0800

    refactor: use utility function

--- app/src/actions/scaleNoteTime.ts
+++ app/src/actions/scaleNoteTime.ts
@@ -1,11 +1,12 @@
+import { getSelectedNotes, getUnselectedNotes, sortNotes } from "@/utils/notes";
 import { PianoRollData } from "react-piano-roll";
 
 export function scaleNoteTime(
   scale: number,
 ): (data: PianoRollData, set: (notes: Partial<PianoRollData>) => void) => void {
   return function (data: PianoRollData, set: (notes: Partial<PianoRollData>) => void) {
-    let selectedNotes = data.notes.filter((note) => note.isSelected).sort((a, b) => a.tick - b.tick);
-    let unselectedNotes = data.notes.filter((note) => !note.isSelected);
+    let selectedNotes = sortNotes(getSelectedNotes(data));
+    let unselectedNotes = getUnselectedNotes(data);
     const startingTick = selectedNotes[0].tick;
     selectedNotes.forEach((_, i) => {
       selectedNotes[i].tick = startingTick + (selectedNotes[i].tick - startingTick) * scale;
