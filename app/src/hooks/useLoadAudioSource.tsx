f3677008350b4f5908a265c8004a9c2619a7572f
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-04-10 03:50:11 +0800

    fix: useLoadAudioSource does not change url when loading

--- app/src/hooks/useLoadAudioSource.tsx
+++ app/src/hooks/useLoadAudioSource.tsx
@@ -3,13 +3,18 @@ import { BlobWithUrl } from "./useBlobUrl";
 
 export function useLoadAudioSource(audioRef: React.RefObject<HTMLAudioElement>, audioSource: BlobWithUrl | null) {
   useEffect(() => {
+    if (!audioRef.current) {
+      return;
+    }
     if (audioSource) {
-      if (audioRef.current?.paused) {
-        audioRef.current?.load();
+      if (audioRef.current.paused) {
+        audioRef.current.src = audioSource.url;
+        audioRef.current.load();
       } else {
-        audioRef.current?.pause();
-        audioRef.current?.load();
-        audioRef.current?.play();
+        audioRef.current.src = audioSource.url;
+        audioRef.current.pause();
+        audioRef.current.load();
+        audioRef.current.play();
       }
     }
   }, [audioSource]);
