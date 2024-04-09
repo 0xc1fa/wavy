b662d23285f4959dd0a7bf52c3448e01eff401c3
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-04-10 03:45:09 +0800

    test: set jest config file to ts

--- app/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  verbose: true,
-  preset: 'ts-jest',
-  testEnvironment: "jsdom",
-  testPathIgnorePatterns: ["/node_modules/"],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
---
+++ app/jest.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+import type {Config} from 'jest';
+
+const config: Config = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: "jsdom",
+};
+
+export default config;
