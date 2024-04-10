ccc46b0def52e5b0a976ec0877e4c997ba396c79
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-04-11 00:40:27 +0800

    refactor: refactor useHandleSpaceDown

--- packages/react-piano-roll/src/components/handlers/useHandleSpaceDown.tsx
+++ packages/react-piano-roll/src/components/handlers/useHandleSpaceDown.tsx
@@ -1,41 +1,34 @@
 import { useEventListener } from "@/hooks/useEventListener";
-import { MutableRefObject, RefObject, useEffect, useRef } from "react";
+import { RefObject, useRef } from "react";
 
 export function useHandleSpaceDown<T extends HTMLElement>(ref: RefObject<T>) {
-  let spaceDown = useRef(false);
+  const spaceDown = useRef(false);
   const isPlaying = useRef(false);
 
-  function createEvent(type: string) {
-    return new CustomEvent(type, {
-      bubbles: true,
-      cancelable: true,
-      detail: {},
-    });
+  function togglePlay() {
+    if (isPlaying.current) {
+      ref.current!.dispatchEvent(new CustomEvent("pause"));
+    } else {
+      ref.current!.dispatchEvent(new CustomEvent("play"));
+    }
+    isPlaying.current = !isPlaying.current;
   }
 
-  const spaceDownHandler = (event: KeyboardEvent) => {
+  useEventListener("keydown", (event: KeyboardEvent) => {
     if (event.code === "Space") {
       event.preventDefault();
       if (spaceDown.current) {
         return;
       }
-      if (isPlaying.current) {
-        ref.current!.dispatchEvent(createEvent("pause"));
-        isPlaying.current = false;
-      } else {
-        ref.current!.dispatchEvent(createEvent("play"));
-        isPlaying.current = true;
-      }
       spaceDown.current = true;
+      togglePlay();
     }
-  };
-  const spaceUpHandler = (event: KeyboardEvent) => {
+  }, ref);
+
+  useEventListener("keyup", (event: KeyboardEvent) => {
     if (event.code === "Space") {
       event.preventDefault();
       spaceDown.current = false;
     }
-  };
-
-  useEventListener("keydown", spaceDownHandler, ref);
-  useEventListener("keyup", spaceUpHandler, ref);
+  }, ref);
 }
